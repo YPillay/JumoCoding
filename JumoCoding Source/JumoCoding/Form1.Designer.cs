d18fc263302a1505a2c815fbb30d316912ca65bd
YPillay <yeglanpillay@gmail.com>
2017-04-30 20:45:10 +0200

    Add files via upload

---
+++ JumoCoding Source/JumoCoding/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace JumoCoding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_importCSV = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_importCSV
+            // 
+            this.btn_importCSV.Location = new System.Drawing.Point(148, 129);
+            this.btn_importCSV.Name = "btn_importCSV";
+            this.btn_importCSV.Size = new System.Drawing.Size(234, 47);
+            this.btn_importCSV.TabIndex = 0;
+            this.btn_importCSV.Text = "Import CSV";
+            this.btn_importCSV.UseVisualStyleBackColor = true;
+            this.btn_importCSV.Click += new System.EventHandler(this.btn_importCSV_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please click the below button to start the CSV conversion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Please ensure csv is formatted correctly before uploading";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 315);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_importCSV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_importCSV;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JumoCoding Source/JumoCoding/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JumoCoding
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string,int> networks ;
+        Dictionary<string, int> products;
+        List<string> months;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Onclick event, Main event
+        private void btn_importCSV_Click(object sender, EventArgs e)
+        {
+            //initialize global variables
+            networks = new Dictionary<string,int>();
+            products = new Dictionary<string, int>();
+            months = new List<string>();
+            // Import csv files using browser
+            // Filter out other file types
+            //openFileDialog1.Title = "Select .csv to import";
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
+            try
+            {
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //run fuction to process values in CSV
+                    List<Loan> loans = ImportCSV(openFileDialog1.FileName);
+                    MessageBox.Show("Import Successful");
+                    Aggregate(loans);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error importing CSV \n" + exception.ToString());
+            }
+
+
+        }
+        #endregion
+
+        #region Functions
+
+        //Import Loans from CSV, Returns list of loans
+        public List<Loan> ImportCSV(string filename)
+        {
+            List<Loan> loans = new List<Loan>();
+            StreamReader sr = new StreamReader(filename);
+            //remove 1st line as we know the contents of the file 
+            sr.ReadLine();
+            //initialize counts
+            int c_net = 0;
+            int c_prod = 0;
+            while (!sr.EndOfStream)
+            {
+                //add loan to list of loans
+
+                string line = sr.ReadLine();
+                string[] vals = line.Split(',');
+                Loan tmp = new Loan(vals);
+                loans.Add(tmp);
+                //populate lists to find number of unique products,networks and months
+                if (!networks.ContainsKey(tmp.network))
+                {
+                    networks.Add(tmp.network,c_net);
+                    c_net++;
+                }
+                if (!products.ContainsKey(tmp.product))
+                {
+                    products.Add(tmp.product,c_prod);
+                    c_prod++;
+                }
+                if (!months.Contains(tmp.date.Month.ToString()))
+                {
+                    months.Add(tmp.date.Month.ToString());
+                }
+            }
+            sr.Close();
+            return loans;
+        }
+
+        //sort loans into arrays and into dictionaries
+        public Dictionary<string,decimal[,,]> Aggregate(List<Loan> loans)
+        {
+            
+            //TODO : make sure arrays are not empty
+            //create an dictionary which will contain the arrays for the different months
+            Dictionary<string,decimal[,,]> d_Agg = new Dictionary<string,decimal[,,]>();
+            for (int i = 0; i < months.Count; i++)
+            {
+                //create arrays for the networks and products per month
+                
+                decimal[,,] arr = new decimal[networks.Count, products.Count,2];
+
+                foreach (Loan l in loans)
+                {
+                    if (l.date.Month.ToString()==months[i]){
+                        //get the network from the current loan
+                        string network = l.network;
+                        int pos_net = networks[network];
+                        //get the product for the curent loan
+                        string product = l.product;
+                        int pos_prod = products[product];
+                        arr[pos_net,pos_prod,0]=l.amount;
+                        //increment count
+                        decimal count = arr[pos_net,pos_prod,1];
+                        arr[pos_net, pos_prod, 1]=count+1;
+                    }
+                    
+                }
+
+                d_Agg.Add(months.ElementAt(i),arr);
+            }
+            Output(d_Agg);
+            return d_Agg;
+        }
+
+        //output to csv
+        public void Output(Dictionary<string,decimal[,,]> d_agg)
+        {
+            //get save location
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Comma Seperated values|*.csv";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            //write to csv file
+            var csv = new StringBuilder();
+            string temp = "";
+            foreach (KeyValuePair<string, decimal[,,]> entry in d_agg)
+            {
+                //go through the dictionary of the different months and pull the arrays of loans
+                decimal[,,] arr = entry.Value;
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        //loop through the array 
+                            temp += getNetworkName(i) +"," ;
+                            temp += getProductName(j) + ",";
+                            temp += CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(int.Parse(entry.Key))+",";
+                            temp += arr[i, j, 0] +",";
+                            temp += arr[i, j, 1];
+                            csv.AppendLine(temp);
+                            temp = "";
+
+                    }
+                }
+
+                
+                
+            }
+            //save file
+
+            if (saveFileDialog1.FileName != "")
+            {
+                File.WriteAllText(saveFileDialog1.FileName, csv.ToString());
+                MessageBox.Show("Output csv created successfully");
+            }
+            else
+            {
+                MessageBox.Show("Output csv could not be created");
+            }
+        }
+
+        //return product name from index
+        public string getProductName(int c)
+        {
+            foreach (KeyValuePair<string, int> entry in products)
+            {
+                if (entry.Value == c)
+                {
+                    return entry.Key;
+                }
+            }
+            return ("error");
+
+        }
+        //return network name from index
+        public string getNetworkName(int c){
+            foreach (KeyValuePair<string, int> entry in networks)
+            {
+                if (entry.Value == c) {
+                    return entry.Key;
+                }
+            }
+            return ("error");
+
+        }
+        #endregion
+    }
+
+
+    #region Loan Class
+    //Loan class contains details of a specific loan
+    public class Loan
+    {
+        #region  variables
+        public string network { get; set; }
+        public DateTime date { get; set; }
+        public string product { get; set; }
+        public decimal amount { get; set; }
+        #endregion
+
+        #region Constructor
+        //construct loan object, Remove ' character when necessary
+        public Loan(string[] vals)
+        {
+            network = vals[1].Trim('\'');
+            string format = "dd-MMM-yyyy";
+            //convert from string to datetime for ease of use
+            date = DateTime.ParseExact(vals[2].Trim('\''), format, CultureInfo.InvariantCulture, DateTimeStyles.None);
+            product = vals[3].Trim('\'');
+            // "." is culture specific. Invariant culture avoids errors
+            amount = decimal.Parse(vals[4], CultureInfo.InvariantCulture);
+        }
+        #endregion
+
+    #endregion
+    }
+}
